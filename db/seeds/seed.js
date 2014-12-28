87d39e3bb76405bae1c9fdf64900e15822c0ffd7
Mike Calvanese <mfcalvanese@gmail.com>
2014-12-28 14:15:13 -0500

    Added some seed data

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -17,11 +17,12 @@ exports.seed = function(knex, Promise) {
     }).then(function() {
         return knex('things').insert([
             {type: 'hammer', deleted: false, user_id: userIds[0]},
-            {type: 'hammer', deleted: true, user_id: userIds[0]},
-            {type: 'hammer', deleted: false },
+            {type: 'drill', deleted: true, user_id: userIds[0]},
             {type: 'nail', deleted: false, user_id: userIds[0]},
             {type: 'bolt', deleted: true, user_id: userIds[1]},
-            {type: 'saw', deleted: true, user_id: userIds[1]}
+            {type: 'saw', deleted: true, user_id: userIds[1]},
+            {type: 'wrench', deleted: false },
+            {type: 'pliers', deleted: false }
         ]);
     });
 };
