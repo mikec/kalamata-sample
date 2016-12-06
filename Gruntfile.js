687195a4dfa83f3e13b18503e85645eb6b04329f
Vaclav Klecanda <vencax77@gmail.com>
2016-12-06 17:33:36 +0100

    pg removed, automigration

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,15 +4,6 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
 
-        exec: {
-            create_db: 'createdb kalamata_sample',
-            create_db_user: 'createuser kalamata_sample_user',
-            drop_db: 'dropdb kalamata_sample',
-            drop_user: 'dropuser kalamata_sample_user',
-            knex_migrate: 'knex migrate:latest',
-            knex_seed: 'knex seed:run'
-        },
-
         express: {
             server: {
                 options: {
@@ -30,18 +21,6 @@ module.exports = function(grunt) {
         }
 
     });
-
-    grunt.registerTask('setup', [
-        'exec:create_db',
-        'exec:create_db_user',
-        'exec:knex_migrate',
-        'exec:knex_seed'
-    ]);
-
-    grunt.registerTask('teardown', [
-        'exec:drop_db',
-        'exec:drop_user'
-    ]);
 
     grunt.registerTask('default', [
         'express',
